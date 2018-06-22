7db6d9676e9e5e1f3bb3c0d30adb95cdf0c237e4
Snehal Jadhav <snehal.jadhav@silicus.com>
2018-06-22 15:23:22 +0530

    Initial commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microcharts.Droid;
+using Microcharts;
+
+namespace ChartsComponent.Droid
+{
+    [Activity(Label = "ChartsComponent", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+                        var charts = Dataa.CreateQuickstart();
+
+            FindViewById<ChartView>(Resource.Id.chartView1).Chart = charts[0];
+            FindViewById<ChartView>(Resource.Id.chartView2).Chart = charts[1];
+            FindViewById<ChartView>(Resource.Id.chartView3).Chart = charts[2];
+            FindViewById<ChartView>(Resource.Id.chartView4).Chart = charts[3];
+            FindViewById<ChartView>(Resource.Id.chartView5).Chart = charts[4];
+            FindViewById<ChartView>(Resource.Id.chartView6).Chart = charts[5];
+
+        }
+    }
+}
+
